b7255c8f739d40e94f2bd71e80971a839a4520d7
Mena Ehab <127118141+commanderYT123@users.noreply.github.com>
2024-10-07 20:29:03 +0300

    Create admin middleware

--- routes/admin.php
+++ routes/admin.php
@@ -5,6 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('admin')->group(function () {
     Route::get('/home', function () {
         return view('admin.index');
-    });
+    })->middleware('isAdmin');
     require __DIR__ . '/admin_auth.php';
 });
