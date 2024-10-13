6f4bbdafa21aebfd226d11d601ba5bd388d8faa6
Mena Ehab <127118141+commanderYT123@users.noreply.github.com>
2024-10-14 01:14:11 +0300

    Create permissions using seeder

---
+++ database/seeders/PermissionSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class PermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $permissions = [
+            'add_user',
+            'show_user',
+            'edit_user',
+            'delete_user',
+        ];
+        foreach ($permissions as $permission) {
+            Permission::updateOrCreate(['name' => $permission], [
+                'name' => $permission,
+                'guard_name' => "admin",
+            ]);
+        }
+    }
+}
